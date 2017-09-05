493a700478cf8c808f7720c56eb3537f06c4fa44
oliver.dendis <oliver.dendis@proofreason.com>
2017-09-05 02:38:47 +0200

    Completly injected data from db to view

--- resources/views/frontend/properties2.blade.php
+++ resources/views/frontend/properties2.blade.php
@@ -19,16 +19,16 @@
         <div class="cbp-item sale">
           <div class="property_item">
             <div class="image">
-              <a href={{$property->url}}><img src="images/listing1.jpg" alt="latest property" class="img-responsive"></a> <!-- hlavny obrazok -->
+              <a href=properties/{{$property->id}}><img src="images/listing1.jpg" alt="latest property" class="img-responsive"></a> <!-- hlavny obrazok -->
 
               <div class="price clearfix">
                 <span class="tag pull-right">{{$property->price}} € / mesiac</span> <!-- Cena -->
               </div>
-              <span class="tag_t">     </span>  <!-- typ ubytka podnajom / spolubyvanie atd -->
+              <span class="tag_t">  {{$types->where('id',$property->type)->first()->name}}   </span>  <!-- typ ubytka podnajom / spolubyvanie atd -->
             </div>
             <div class="proerty_content">
               <div class="proerty_text">
-                <h3><a href={{$property->url}}>{{$property->title}}</a></h3><!-- hlavný nadpis -->
+                <h3><a href=properties/{{$property->id}}>{{$property->title}}</a></h3><!-- hlavný nadpis -->
                 <p>{{$property->address_street}}, {{$property->address_city}}</p> <!-- Lokalita -->
               </div>
               <div class="property_meta transparent">
@@ -37,7 +37,7 @@
               </div>
               <div class="property_meta transparent bottom30">
                 <span><i class="icon-select-an-objecto-tool"></i>{{$property->area}} m2</span>  <!-- Výmera -->
-                <span><i class="icon-bed"></i>2-izbový byt</span>   <!-- Dispozícia -->
+                <span><i class="icon-bed"></i>{{$dispositions->where('id',$property->disposition)->first()->name}} </span>   <!-- Dispozícia -->
                 <span><i class="icon-calendar"></i>od {{date('d.m.Y', strtotime($property->available_from))}}</span>   <!-- Dostupný od -->
               </div>
 
